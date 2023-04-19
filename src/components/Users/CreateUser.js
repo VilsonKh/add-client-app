ccf04d32435c36c721d9188e9e2ef4c8dffa4b7c
Vilson <khvilson@yandex.ru>
2023-04-19 23:29:10 +0300

    added popup

--- src/components/Users/CreateUser.js
+++ src/components/Users/CreateUser.js
@@ -3,18 +3,28 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import styles from "./CreateUser.module.css";
+import ErrorModal from "../UI/ErrorModal";
 
 const CreateUser = (props) => {
 	const [inputName, setInputName] = useState("");
 	const [inputAge, setInputAge] = useState("");
+  const [error, setError] = useState()
 
 	const createUserHandler = (event) => {
 		event.preventDefault();
 		if (inputName.trim().length === 0 || inputAge.trim().length === 0) {
+      setError({
+        title: "Некорректный ввод",
+        message: "Эти поля не могут быть пустыми"
+      })
 			return;
 		}
 
     if(+inputAge < 1) {
+      setError({
+        title: "Некорректный возраст",
+        message: "Возраст должен быть больше 0"
+      })
       return;
     }
 		props.onCreateUser(inputName,inputAge)
@@ -30,16 +40,24 @@ const CreateUser = (props) => {
 		setInputAge(event.target.value);
 	};
 
+  const errorHandler = () => {
+    setError(false)
+  }
+
 	return (
-		<Card className={styles.input}>
-			<form onSubmit={createUserHandler}>
-				<label htmlFor="name">Имя</label>
-				<input id="name" type="text" onChange={nameChangeHandler} value={inputName} />
-				<label htmlFor="age">Возраст</label>
-				<input id="age" type="number" onChange={ageChangeHandler} value={inputAge} />
-				<Button type="submit">Добавить пользователя</Button>
-			</form>
-		</Card>
+    <div>
+      {error && <ErrorModal onCloseModal={errorHandler} title={error.title} message={error.message}></ErrorModal>}
+
+  		<Card className={styles.input}>
+  			<form onSubmit={createUserHandler}>
+  				<label htmlFor="name">Имя</label>
+  				<input id="name" type="text" onChange={nameChangeHandler} value={inputName} />
+  				<label htmlFor="age">Возраст</label>
+  				<input id="age" type="number" onChange={ageChangeHandler} value={inputAge} />
+  				<Button type="submit">Добавить пользователя</Button>
+  			</form>
+  		</Card>
+    </div>
 	);
 };
 
